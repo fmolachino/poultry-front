6f986246f3b5190edea47d00ea05838e54b4b2ab
fmolachino <fmolachino@gmail.com>
2024-12-25 16:21:02 -0300

    added user management barebone

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//User components
+import { LoginComponent } from './user-management/login/login.component';
+import { RegisterComponent } from './user-management/register/register.component';
+import { UserListComponent } from './user-management/user-list/user-list.component';
+
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'users', component: UserListComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
